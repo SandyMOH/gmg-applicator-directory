6c73013d6a449e8de5e106ed096b60e500da42c7
Sandy Mohammad <sandymohammad99@gmail.com>
2026-09-10 12:58:20 +0700

    uptick: 3.2.0

--- applicator-directory.php
+++ applicator-directory.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Applicator Directory
  * Plugin URI:        https://thermal-xr.com
  * Description:       Certified applicator directory with 3-tab search (All / Certified Sprayers / Spray Hubs). Uses ACF + Google Maps. Shortcode: [applicator_directory]
- * Version:           3.1.1
+ * Version:           3.2.0
  * Author:            Sandy Mohammad
  * License:           GPL v2 or later
  * Text Domain:       applicator-directory
@@ -11,7 +11,7 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-define( 'APPDIR_VERSION', '3.1.1' );
+define( 'APPDIR_VERSION', '3.2.0' );
 define( 'APPDIR_PATH', plugin_dir_path( __FILE__ ) );
 define( 'APPDIR_URL', plugin_dir_url( __FILE__ ) );
 
--- assets/js/applicator.js
+++ assets/js/applicator.js
@@ -1,5 +1,5 @@
 /**
- * Applicator Directory v3.1.1
+ * Applicator Directory v3.2.0
  * 3-tab layout: All / Certified Sprayers / Spray Hubs
  * Uses Google Maps API
  * Compatible with Elementor, Divi, and Gutenberg
